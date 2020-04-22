765186f0a582a28955a1b3f0c2997dbcbd7b4c72
Anna Lux <anna.watt@flatironschool.com>
2020-04-22 13:00:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,5 @@
 def sort_array_asc(array)
-  array.sort do |a,b|
-    a <=> b
-  end
+  array.sort
 end
 
 def sort_array_desc(array)
@@ -17,19 +15,12 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  first = array[0]
-  second = array[1]
-  third = array[2]
-  3.times do
-    array.pop
-  end
-  array.unshift(second)
-  array.unshift(third)
-  array.unshift(first)
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
-  array.reverse!
+  array.reverse
 end
 
 def kesha_maker(array)
@@ -53,10 +44,10 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index != 1
+    if index == 1
+      element
+    else
       element << "s"
-    elsif index == 1
-      element  
     end
   end
 end
